23edddcf1ed6ec610e05a014eb27278aa2d37981
John Paxton <pax@speedingplanet.com>
2021-04-28 11:42:08 -0400

    Exercise

--- main/day-3/users-table-main.js
+++ main/day-3/users-table-main.js
@@ -1,3 +1,13 @@
 import { users } from '../../node_modules/@speedingplanet/rest-server/index.js';
 
 console.log('users:', users);
+
+/*
+  Render the users who are people to the table body
+  Get a reference to the tbody
+  Filter the users for userType === 'person'
+  Iterate over the users array
+  Generate a table row <tr></tr>
+  Print firstName, lastName, address.city, and address.state, each as a td cell <td></td>
+  (Get firstName and lastName from displayName)
+*/
