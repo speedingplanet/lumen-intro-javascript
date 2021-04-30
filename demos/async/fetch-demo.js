44729fb477eccd162144392265f6f63fb97bdb1b
John Paxton <pax@speedingplanet.com>
2021-04-30 12:56:34 -0400

    Handing 404s

--- demos/async/fetch-demo.js
+++ demos/async/fetch-demo.js
@@ -10,6 +10,13 @@ document
     });
   });
 
+document.querySelector('button.btn-warning').addEventListener('click', () => {
+  let userPromise = fetchDataWithBadResponse();
+  userPromise
+    .then((user) => console.log('user:', user))
+    .catch((error) => console.error('Request error:', error));
+});
+
 function fetchData() {
   let responsePromise = fetch('http://localhost:8000/api/zippay/v1/users/201');
   // return fetch('http://localhost:8000/api/zippay/v1/users/201');
@@ -25,4 +32,21 @@ function fetchDataTerse() {
   return fetch(
     'http://localhost:8000/api/zippay/v1/users/201'
   ).then((response) => response.json());
+}
+
+function fetchDataWithBadResponse() {
+  return fetch('http://localhost:8000/api/zippay/v1/users/501').then(
+    (response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject({
+          message: 'HTTP error',
+          response,
+          status: response.status,
+          statusText: response.statusText,
+        });
+      }
+    }
+  );
 }
